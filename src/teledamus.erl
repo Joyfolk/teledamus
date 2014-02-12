66531859d1700e22a91303711e07c23023f370f6
Joyfolk <gfhfkm>
2014-02-12 16:06:58 +0400

    bugfixes

--- src/teledamus.erl
+++ src/teledamus.erl
@@ -8,8 +8,7 @@
 
 -include_lib("native_protocol.hrl").
 
--export([get_connection/0, get_connection/1, release_connection/1, release_connection/2, new_stream/2, new_stream/1, release_stream/2, release_stream/1,
-         options/2, options/1, query/2, query/3, query/4, query/5, prepare_query/2, prepare_query/3, prepare_query/4,
+-export([get_connection/0, get_connection/1, release_connection/1, release_connection/2, options/2, options/1, query/2, query/3, query/4, query/5, prepare_query/2, prepare_query/3, prepare_query/4,
          execute_query/2, execute_query/3, execute_query/4, batch_query/2, batch_query/3, batch_query/4, subscribe_events/2, subscribe_events/3, start/0, stop/0]).
 
 -define(DEFAULT_TIMEOUT, 5000).
@@ -19,14 +18,14 @@
 %%% @end
 -spec start() -> ok | {error, any()}.
 start() ->
-	application:start(teledamus).
+  application:start(teledamus).
 
 %%% @doc
 %%% Stop application
 %%% @end
 -spec stop() -> ok | {error, any()}.
 stop() ->
-	application:stop(teledamus).
+  application:stop(teledamus).
 
 %%% @doc
 %%% Get connection to Cassandra
@@ -35,7 +34,7 @@ stop() ->
 %%% @end
 -spec get_connection() -> connection() | {error, any()}.
 get_connection() ->
-	teledamus_srv:get_connection(?DEFAULT_TIMEOUT).
+  teledamus_srv:get_connection(?DEFAULT_TIMEOUT).
 
 
 %%% @doc
@@ -56,7 +55,7 @@ get_connection(Timeout) ->
 %%% @end
 -spec release_connection(connection()) -> 'ok'.
 release_connection(Connection) ->
-	teledamus_srv:release_connection(Connection, ?DEFAULT_TIMEOUT).
+  teledamus_srv:release_connection(Connection, ?DEFAULT_TIMEOUT).
 
 %%% @doc
 %%% Release connection
@@ -69,57 +68,14 @@ release_connection(Connection) ->
 release_connection(Connection, Timeout) ->
   teledamus_srv:release_connection(Connection, Timeout).
 
-
-%%% @doc
-%%% Create new stream for given connection
-%%%
-%%% Connection - connection to cassandra
-%%% Result - stream | {error, Reason}
-%%% @end
--spec new_stream(connection()) -> stream() | {error, any()}.
-new_stream(Connection) ->
-  connection:new_stream(Connection, ?DEFAULT_TIMEOUT).
-
-%%% @doc
-%%% Create new stream for given connection
-%%%
-%%% Connection - connection to cassandra
-%%% Timeout - operation timeout
-%%% Result - stream | {error, Reason}
-%%% @end
--spec new_stream(connection(), timeout()) -> stream() | {error, any()}.
-new_stream(Connection, Timeout) ->
-  connection:new_stream(Connection, Timeout).
-
-
-%%% @doc
-%%% Release stream
-%%%
-%%% Stream - ...
-%%% Result - stream | {error, Reason}
-%%% @end
--spec release_stream(stream()) -> ok | {error, any()}.
-release_stream(Stream) ->
-  connection:release_stream(Stream, ?DEFAULT_TIMEOUT).
-
-
-%%% @doc
-%%% Release stream
-%%%
-%%% Stream - ...
-%%% Timeout - operation timeout
-%%% Result - stream | {error, Reason}
-%%% @end
--spec release_stream(stream(), timeout()) -> ok | {error, any()}.
-release_stream(Stream, Timeout) ->
-  connection:release_stream(Stream, Timeout).
-
 %%% @doc
 %%% Request DB for options
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection or Stream - connection to DB, as returned from get_connection() or stream
 %%% @end
--spec options(connection()) -> timeout | error() | options().
+-spec options(connection() | stream()) -> timeout | error() | options().
+options(Stream = #stream{}) ->
+  connection:options(Stream, ?DEFAULT_TIMEOUT);
 options(Connection) ->
   connection:options(Connection, ?DEFAULT_TIMEOUT).
 
@@ -127,34 +83,40 @@ options(Connection) ->
 %%% @doc
 %%% Request DB for options
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection or Stream - connection to DB, as returned from get_connection() or stream
 %%% Timeout - the number of milliseconds before operation times out.
 %%% @end
--spec options(connection(), timeout()) -> timeout | error() | options().
+-spec options(connection() | stream(), timeout()) -> timeout | error() | options().
+options(Stream = #stream{}, Timeout) ->
+  stream:options(Stream, Timeout);
 options(Connection, Timeout) ->
-	connection:options(Connection, Timeout).
+  connection:options(Connection, Timeout).
 
 
 %%% @doc
 %%% Execute query with default parameters & default timeout
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection or Stream - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Result - result of query or error or timeout.
 %%% @end
--spec query(connection(), string()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec query(connection() | stream(), string()) -> timeout | ok | error() | result_rows() | schema_change().
+query(Stream = #stream{}, Query) ->
+  stream:query(Stream, Query, #query_params{}, ?DEFAULT_TIMEOUT);
 query(Connection, Query) ->
   connection:query(Connection, Query, #query_params{}, ?DEFAULT_TIMEOUT).
 
 %%% @doc
 %%% Execute query with default parameters
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection or Stream - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec query(connection(), string(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec query(connection() | stream(), string(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+query(Stream = #stream{}, Query, Timeout) ->
+  stream:query(Stream, Query, #query_params{}, Timeout);
 query(Connection, Query, Timeout) ->
   connection:query(Connection, Query, #query_params{}, Timeout).
 
@@ -162,27 +124,31 @@ query(Connection, Query, Timeout) ->
 %%% @doc
 %%% Execute query
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection or stream - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Params - query parameters
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec query(connection(), string(), query_params(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec query(connection()| stream(), string(), query_params(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+query(Stream = #stream{}, Query, Params, Timeout) ->
+  stream:query(Stream, Query, Params, Timeout);
 query(Connection, Query, Params, Timeout) ->
-	connection:query(Connection, Query, Params, Timeout).
+  connection:query(Connection, Query, Params, Timeout).
 
 %%% @doc
 %%% Execute query
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Params - query parameters
 %%% Timeout - the number of milliseconds before operation times out.
 %%% UseCache - use cached preparestatements
 %%% Result - result of query or error or timeout.
 %%% @end
--spec query(connection(), string(), query_params(), timeout(), boolean()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec query(connection() | stream(), string(), query_params(), timeout(), boolean()) -> timeout | ok | error() | result_rows() | schema_change().
+query(Stream = #stream{}, Query, Params, Timeout, UseCache) ->
+  stream:query(Stream, Query, Params, Timeout, UseCache);
 query(Connection, Query, Params, Timeout, UseCache) ->
   connection:query(Connection, Query, Params, Timeout, UseCache).
 
@@ -190,36 +156,42 @@ query(Connection, Query, Params, Timeout, UseCache) ->
 %%% @doc
 %%% Create prepared statement
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Result - {prepared_query_id() :: binary()} or error or timeout
 %%% @end
--spec prepare_query(connection(), string()) -> timeout | error() | {binary(), metadata(), metadata()}.
+-spec prepare_query(connection() | stream(), string()) -> timeout | error() | {binary(), metadata(), metadata()}.
+prepare_query(Stream = #stream{}, Query) ->
+  stream:prepare_query(Stream, Query, ?DEFAULT_TIMEOUT);
 prepare_query(Connection, Query) ->
   connection:prepare_query(Connection, Query, ?DEFAULT_TIMEOUT).
 
 %%% @doc
 %%% Create prepared statement
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - {prepared_query_id() :: binary()} or error or timeout
 %%% @end
--spec prepare_query(connection(), string(), timeout()) -> timeout | error() | {binary(), metadata(), metadata()}.
+-spec prepare_query(connection() | stream, string(), timeout()) -> timeout | error() | {binary(), metadata(), metadata()}.
+prepare_query(Stream = #stream{}, Query, Timeout) ->
+  stream:prepare_query(Stream, Query, Timeout);
 prepare_query(Connection, Query, Timeout) ->
-	connection:prepare_query(Connection, Query, Timeout).
+  connection:prepare_query(Connection, Query, Timeout).
 
 %%% @doc
 %%% Create prepared statement
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Query - string with CQL query
 %%% Timeout - the number of milliseconds before operation times out.
 %%% UseCache - use cached preparestatements
 %%% Result - {prepared_query_id() :: binary()} or error or timeout
 %%% @end
--spec prepare_query(connection(), string(), timeout(), boolean()) -> timeout | error() | {binary(), metadata(), metadata()}.
+-spec prepare_query(connection() | stream, string(), timeout(), boolean()) -> timeout | error() | {binary(), metadata(), metadata()}.
+prepare_query(Stream = #stream{}, Query, Timeout, UseCache) ->
+  stream:prepare_query(Stream, Query, Timeout, UseCache);
 prepare_query(Connection, Query, Timeout, UseCache) ->
   connection:prepare_query(Connection, Query, Timeout, UseCache).
 
@@ -227,50 +199,58 @@ prepare_query(Connection, Query, Timeout, UseCache) ->
 %%% @doc
 %%% Execute prepared statement with default parameters & default timeout
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% ID - prepared query ID
 %%% Params - query parameters
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec execute_query(connection(), binary()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec execute_query(connection() | stream(), binary()) -> timeout | ok | error() | result_rows() | schema_change().
+execute_query(Stream = #stream{}, ID) ->
+  stream:execute_query(Stream, ID, #query_params{}, ?DEFAULT_TIMEOUT);
 execute_query(Connection, ID) ->
   connection:execute_query(Connection, ID, #query_params{}, ?DEFAULT_TIMEOUT).
 
 %%% @doc
 %%% Execute prepared statement with default parameters
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% ID - prepared query ID
 %%% Params - query parameters
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec execute_query(connection(), binary(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec execute_query(connection() | stream(), binary(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+execute_query(Stream = #stream{}, ID, Timeout) ->
+  stream:execute_query(Stream, ID, #query_params{}, Timeout);
 execute_query(Connection, ID, Timeout) ->
   connection:execute_query(Connection, ID, #query_params{}, Timeout).
 
 %%% @doc
 %%% Execute prepared statement
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% ID - prepared query ID
 %%% Params - query parameters
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec execute_query(connection(), binary(), query_params(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+-spec execute_query(connection() | stream(), binary(), query_params(), timeout()) -> timeout | ok | error() | result_rows() | schema_change().
+execute_query(Stream = #stream{}, ID, Params, Timeout) ->
+  stream:execute_query(Stream, ID, Params, Timeout);
 execute_query(Connection, ID, Params, Timeout) ->
-	connection:execute_query(Connection, ID, Params, Timeout).
+  connection:execute_query(Connection, ID, Params, Timeout).
 
 %%% @doc
 %%% Execute batch query
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Batch - batch query record. Contains list of queries in the batch. It can be prepared statements or simple DML cql queries (INSERT/UPDATE/DELETE, no SELECTs)
 %%% Result - result of query or error or timeout.
 %%% @end
--spec batch_query(connection(), binary()) -> timeout | ok.
+-spec batch_query(connection() | stream(), binary()) -> timeout | ok.
+batch_query(Stream = #stream{}, Batch) ->
+  stream:batch_query(Stream, Batch, ?DEFAULT_TIMEOUT);
 batch_query(Connection, Batch) ->
   connection:batch_query(Connection, Batch, ?DEFAULT_TIMEOUT).
 
@@ -278,26 +258,30 @@ batch_query(Connection, Batch) ->
 %%% @doc
 %%% Execute batch query
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Batch - batch query record. Contains list of queries in the batch. It can be prepared statements or simple DML cql queries (INSERT/UPDATE/DELETE, no SELECTs)
 %%% Timeout - the number of milliseconds before operation times out.
 %%% Result - result of query or error or timeout.
 %%% @end
--spec batch_query(connection(), binary(), timeout()) -> timeout | ok.
+-spec batch_query(connection() | stream(), binary(), timeout()) -> timeout | ok.
+batch_query(Stream = #stream{}, Batch, Timeout) ->
+  stream:batch_query(Stream, Batch, Timeout);
 batch_query(Connection, Batch, Timeout) ->
-	connection:batch_query(Connection, Batch, Timeout).
+  connection:batch_query(Connection, Batch, Timeout).
 
 
 %%% @doc
 %%% Execute batch query
 %%%
-%%% Connection - connection to DB, as returned from get_connection()
+%%% Connection - connection to DB, as returned from get_connection() or stream
 %%% Batch - batch query record. Contains list of queries in the batch. It can be prepared statements or simple DML cql queries (INSERT/UPDATE/DELETE, no SELECTs)
 %%% Timeout - the number of milliseconds before operation times out.
 %%% UseCache - use cached preparestatements
 %%% Result - result of query or error or timeout.
 %%% @end
--spec batch_query(connection(), binary(), timeout(), boolean()) -> timeout | ok.
+-spec batch_query(connection() | stream(), binary(), timeout(), boolean()) -> timeout | ok.
+batch_query(Stream = #stream{}, Batch, Timeout, UseCache) ->
+  stream:batch_query(Stream, Batch, Timeout, UseCache);
 batch_query(Connection, Batch, Timeout, UseCache) ->
   connection:batch_query(Connection, Batch, Timeout, UseCache).
 
@@ -312,7 +296,7 @@ batch_query(Connection, Batch, Timeout, UseCache) ->
 %%% @end
 -spec subscribe_events(connection(), list(), timeout()) -> ok | timeout | error().
 subscribe_events(Connection, EventTypes, Timeout) ->
-	connection:subscribe_events(Connection, EventTypes, Timeout).
+  connection:subscribe_events(Connection, EventTypes, Timeout).
 
 %%% @doc
 %%% Subscribe to Cassandra cluster events.
